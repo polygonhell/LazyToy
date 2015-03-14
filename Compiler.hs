4035e6657a173773d66ce0a95391b4d76897cad4
Rob <polygonhell@gmail.com>
2015-03-14 13:12:38 -0700

    more fixes

--- Compiler.hs
+++ Compiler.hs
@@ -1,9 +1,9 @@
-module Compiler (rewrite, LabelGen(..)) where
+module Compiler (factorArgs, LabelGen(..)) where
 
 import Parser 
 
 import Control.Monad.State.Lazy
-import Debug.Trace
+-- import Debug.Trace
 
 
 data FnDef = FnDef String Int String
@@ -23,18 +23,14 @@ newLabel = do
 
 
 -- Factor outsub expressions
--- factorApp :: FactorState ([Binding], PrgPos)
-
 reWriteArg :: PrgPos -> LabelState ([Bind], PrgPos)
 reWriteArg (PrgApp fn args, pos) = do
   label <- newLabel
   (bindings, prgs) <- reWriteArgs args ([],[])
-  -- trace ("\nrewrittenArgs " ++ (show bindings) ++ "\n" ++ (show prgs) ++ "\n") 
   let bindingPrg = case bindings of 
                       [] -> (PrgApp fn args, pos)
                       _ -> (PrgLet bindings (PrgApp fn prgs, pos), pos)
   return ([(label, bindingPrg)], (PrgId label, pos)) 
-  -- return ([(label, (PrgLet bindings (PrgApp fn prgs, pos), pos))], (PrgId label, pos))
 reWriteArg e = do
   return ([], e)
 
@@ -49,22 +45,18 @@ reWriteArgs (h:t) (pb, pp) = do
 reWriteExpr :: PrgPos -> LabelState PrgPos
 reWriteExpr (PrgApp fn args, pos) = do
   (bindings, prgs) <- reWriteArgs args ([],[])
-  trace ("\nrewrittenExpr " ++ (show bindings) ++ "\n" ++ (show prgs) ++ "\n") 
-    return (PrgLet bindings (PrgApp fn  prgs, pos), pos)
+  return (PrgLet bindings (PrgApp fn  prgs, pos), pos)
 reWriteExpr e = do
   return e
 
 
 
 
--- reWriteExpr ((PrgApp fn args, pos) : t) (pb, pp) = do
-
 
 factorArgs :: PrgPos -> LabelState PrgPos
 factorArgs (PrgTLDef fn args expr, pos) =  do
   (b, p) <- reWriteExpr expr
-  trace ("\n\nTest " ++ (show b) ++ "\n\n" ++ (show p) ++ "\n\n") 
-    return (PrgInt 7, pos)
+  return (PrgTLDef fn args (b, p), pos)
 
 factorArgs e = error ("Error " ++ (show e))
 
@@ -73,9 +65,6 @@ factorArgs e = error ("Error " ++ (show e))
 
 
 
--- compileTLDef :: PrgPos -> Either ParseError FnDef
--- compileTLDef (PrgTLDef name args expr, _) = Right(FnDef name (length args) (show expr))
--- compileTLDef e = error $ "Expected top level definition got " ++ (show e) 
 
 
 
--- Main.hs
+++ Main.hs
@@ -4,25 +4,38 @@ import Parser
 import Lexer 
 import Compiler
 
+import Text.Parsec.Error
 import Control.Monad.State.Lazy
-import Debug.Trace
+-- import Debug.Trace
 
 -- TODO Associativity issue
 testProg :: String
 testProg = unlines 
-  [ "main = (I (J (K 5) (K 6)))"
-  , "I n = n"
+  [ "main = (I (I (I 5)))"
+  , "I x = x"
+  , "K x y = x"
+  , "S x y z = (x z (y z))"
+  , "Iprime x = (S K K x)"
+  , "add a = (plus a)"
+  , "test = (add 3 4)"
   ]
 
-main :: IO ()
-main = putStrLn $ show out where
-  out = do
-    toks <- tokenize "" testProg
+
+compile :: String -> Either ParseError [PrgPos]
+compile input = treeOut where 
+  treeOut = do
+    toks <- tokenize "" input
     tree <- parse toks
-    trace ("Parse = " ++ (show (fst (head tree))))
-      return ()
-    Right $ evalState (factorArgs (head tree)) LabelGen {count = 0}
+    return $ map rewrite tree 
+  -- map rewrite tree where
+  rewrite i = evalState (factorArgs i) LabelGen {count = 0}
 
-    -- Right(tree)
 
-    -- compileTLDef $ head ast 
+k x y = x
+s x y z = x z (y z)
+
+
+main :: IO ()
+main = do 
+  putStrLn $ show $ compile testProg
+  putStrLn $ show $ s k k 5
